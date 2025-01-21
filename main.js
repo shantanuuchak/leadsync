bcde1d3c9d3ce99d8c7566d10e2971e064487843
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-21 08:32:28 +0000

    Print meta fields from csv

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+import fs from "fs";
+import Papa from "papaparse";
+
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 const argv = yargs(hideBin(process.argv)).argv;
-const file = argv["input"];
 
-console.log(file);
+// Get the input file from the command line
+const inputFile = argv["input"];
+const outputFile = argv["output"];
+
+const csvFile = fs.readFileSync(inputFile, "utf8"); // Read the CSV file
+
+// Parse the CSV file
+const csvData = Papa.parse(csvFile, {
+  header: true,
+  dynamicTyping: true,
+  skipEmptyLines: true,
+});
+
+console.log(csvData.meta.fields);
