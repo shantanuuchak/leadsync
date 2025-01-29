213bd27f0c71643b2e20571983991bcc69d94ede
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-29 18:29:45 +0000

    code optimization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to LeadSync will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.6] - 2025-01-29
+
+### Fixed
+
+- General code refactoring
+
+### Changed
+
+- Performed code cleanup and optimization
+
 ## [1.0.5] - 2025-01-29
 
 ### Fixed
--- main.js
+++ main.js
@@ -1,103 +1,21 @@
 #!/usr/bin/env node
-import { createRequire } from "module";
-
-import {
-  intro,
-  outro,
-  isCancel,
-  cancel,
-  text,
-  log,
-  spinner,
-} from "@clack/prompts";
-
-const require = createRequire(import.meta.url);
-const packageJSON = require("./package.json");
-
-// Read CLI Arguments
-import yargs from "yargs/yargs";
-import { hideBin } from "yargs/helpers";
-const argv = yargs(hideBin(process.argv))
-  .option("input", {
-    alias: "i",
-    type: "string",
-    description: "Input CSV file",
-  })
-  .option("output", {
-    alias: "o",
-    type: "string",
-    description: "Output CSV file for clean data",
-  })
-  .option("report", {
-    alias: "r",
-    type: "string",
-    description: "Output CSV file for error reports",
-  })
-  .help("h")
-  .alias("h", "help")
-  .version(packageJSON.version)
-  .alias("v", "version")
-  .parse();
-
+import { intro, spinner, log, outro } from "@clack/prompts";
 import { readCSV, writeCSV } from "./lib/io.js";
-import service from "./lib/core.js";
+import prompt from "./lib/prompt.js";
+import core from "./lib/core.js";
 
 // Starting the main application
 async function main() {
-  intro(`Lead Sync App`);
+  intro(`LeadSync App validate your LinkedIn leads CSV file.`);
 
   const s = spinner();
 
-  let input, output, report;
-
-  // If CLI arguments are provided, use them
-  if (argv.input && argv.output && argv.report) {
-    input = argv.input;
-    output = argv.output;
-    report = argv.report;
-  } else {
-    // Otherwise, use interactive prompts
-    input = await text({
-      message: "What is your input file?",
-      defaultValue: "leads.csv",
-      validate: (value) => {
-        if (!value.toLowerCase().endsWith("csv")) {
-          return "Only csv files are supported.";
-        }
-      },
-    });
-
-    output = await text({
-      message: "What is your output file?",
-      defaultValue: "final.csv",
-      validate: (value) => {
-        if (!value.toLowerCase().endsWith("csv")) {
-          return "Only csv files are supported.";
-        }
-      },
-    });
-
-    report = await text({
-      message: "What is your report file?",
-      defaultValue: "report.csv",
-      validate: (value) => {
-        if (!value.toLowerCase().endsWith("csv")) {
-          return "Only csv files are supported.";
-        }
-      },
-    });
-
-    // Check for cancel
-    if (isCancel(input) || isCancel(output) || isCancel(report)) {
-      cancel("Operation cancelled.");
-      process.exit(1);
-    }
-  }
+  const { input, output, report } = await prompt();
 
   s.start("Starting file validation...");
 
   const csvData = readCSV(input);
-  const [clean, errors] = service(csvData.body);
+  const [clean, errors] = core(csvData.body);
 
   // Create the clean file
   writeCSV(output, clean);
